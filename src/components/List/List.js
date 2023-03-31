08b881377bac029b51c536684a1e3966ce4d4dda
Pawel Rzeznicki <p.rzeznicky@gmail.com>
2023-03-31 20:53:26 +0200

    Init commit

---
+++ src/components/List/List.js
@@ -0,0 +1,23 @@
+const List = () => {
+  return (
+    <div>
+        <header>
+            <h2>Things to do<span>soon</span></h2>
+            <p>Interesting things I want to check out</p>
+        </header>
+        <section>
+            <article>
+                <h3>Books</h3>
+            </article>
+            <article>
+                <h3>Movies</h3>
+            </article>
+            <article>
+                <h3>Games</h3>
+            </article>
+        </section>
+    </div>  
+    );
+};
+
+export default List;
