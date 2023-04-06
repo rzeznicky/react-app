24db49fef3e28882ee352e1f00415a9bdc17f49f
Pawel Rzeznicki <p.rzeznicky@gmail.com>
2023-04-06 19:52:32 +0200

    Adding SearchForm input string to reducer function

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,11 +1,24 @@
-import styles from './SearchForm.module.scss'
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import TextInput from '../TextInput/TextInput';
 import Button from '../Button/Button';
+import styles from './SearchForm.module.scss';
 
 const SearchForm = () => {
+
+    const [searchString, setSearchString] = useState('');
+
+    const dispatch = useDispatch();
+
+    const searchHandle = (e) => {
+        e.preventDefault();
+        dispatch({ type: 'UPDATE_SEARCHSTRING', payload: searchString });
+        setSearchString('');
+    }
+
     return (
-        <form className={styles.searchForm}>
-            <TextInput placeholder="Search..." />
+        <form className={styles.searchForm} onSubmit={searchHandle}>
+            <TextInput value={searchString} onChange={e => setSearchString(e.target.value)} placeholder="Search..." />
             <Button>
                 <span className="fa fa-search" />
             </Button>
--- src/redux/store.js
+++ src/redux/store.js
@@ -7,6 +7,8 @@ const reducer = (state, action) => {
       return { ...state, columns: [...state.columns, action.payload] };
     case 'ADD_CARD':
       return {...state, cards: [...state.cards, action.payload] };
+    case 'UPDATE_SEARCHSTRING':
+      return { ...state, searchString: action.payload };
     default:
       return state;
   }
