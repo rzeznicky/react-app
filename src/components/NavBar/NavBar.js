a60fce01f9eadb6fc5b04403850e241e76c2dac2
Pawel Rzeznicki <p.rzeznicky@gmail.com>
2023-04-07 20:22:53 +0200

    Modify links display with Link and NavLink router components in NavBar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,14 +1,19 @@
+import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import styles from './NavBar.module.scss';
 
 const NavBar = () => {
     return (
         <nav className={styles.nav}>
             <div className={styles.navBar}>
-                <a href='/'><span className='fa fa-bars'></span></a>
+                <Link to="/"><span className='fa fa-bars'></span></Link>
                 <ul>
-                    <li><a href='/'>Home</a></li>
-                    <li><a href='/favorite'>Favorite</a></li>
-                    <li><a href='/about'>About</a></li>
+                    <li><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined}
+                        to="/">Home</NavLink></li>
+                    <li><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined}
+                        to="/favorite">Favorite</NavLink></li>
+                    <li><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined}
+                        to="/about">About</NavLink></li>
                 </ul>
             </div>
         </nav>
