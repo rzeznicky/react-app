a11326fdc912fae3fbcc8fa605e7f2f3d6a22bdc
Pawel Rzeznicki <p.rzeznicky@gmail.com>
2023-04-07 20:09:52 +0200

    Add displaying subpages with navbar links and empty 404 subpage for wrong address

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import Hero from './components/Hero/Hero';
-import SearchForm from './components/SearchForm/SearchForm';
-import List from './components/List/List';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Favorite from './components/Favorite/Favorite';
+import About from './components/About/About';
 import Container from './components/Container/Container';
 import NavBar from './components/NavBar/NavBar';
+import NotFoundPage from './components/NotFoundPage/NotFoundPage';
 
 const App = () => {
   return (
     <main>
       <NavBar />
       <Container>
-        <Hero />
-        <SearchForm />
-        <List />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/favorite" element={<Favorite />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
       </Container>
-    </main>
+ </main>
   );
 };
 
---
+++ src/components/NotFoundPage/NotFoundPage.js
@@ -0,0 +1,9 @@
+import PageTitle from "../PageTitle/PageTitle";
+
+const NotFoundPage = () => {
+    return (
+        <PageTitle>404 not found</PageTitle>
+    )
+}
+
+export default NotFoundPage;
